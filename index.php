bbe85417e3220e332bbb85e3bd8fa167be7c5e54
tidisak <tidisak@gmail.com>
2013-07-26 23:34:13 +0700

    savebuthailand
    Search Engine For Industries

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Search Engine For Industries</title>
+    <link rel="shortcut icon" href="http://www.savebuythailand.com/css/img/favicon.ico" />
+</head>
+<style>
+
+/* for 1024px or less */
+@media screen and (max-width: 1900px) {
+	
+	#container {
+		width: 100%;
+		margin-top:7%;
+	}
+	#body {
+		width: 100%;
+	}
+	
+}
+
+/* for 980px or less */
+@media screen and (max-width: 980px) {
+	
+	#container {
+		width: 100%;
+		margin-top:5%;
+	}
+	#body {
+		width: 100%;
+	}
+	
+}
+
+/* for 700px or less */
+@media screen and (max-width: 700px) {
+
+	#container {
+		width: auto;
+		float: none;
+	}
+	#body {
+		width: auto;
+		float: none;
+	}
+	
+	#search{ width:400px;}
+
+}
+
+/* for 480px or less */
+@media screen and (max-width: 480px) {
+
+	#container {
+		width: auto;
+		float: none;
+	}
+	#body {
+		width: auto;
+		float: none;
+		padding-top:20%;
+	}
+	img {
+		width:90%;
+		margin-left:5%;
+		margin-right:5%;
+	}
+	
+	#search{ width:250px;}	
+}
+
+
+</style>
+<style type="text/css">
+	html
+	{
+		height: 100%;
+	}
+	
+	#content{ padding-bottom:120px;
+	}
+	
+	#footer
+	{
+		position:fixed;
+		bottom: 0;
+		width: 100%;
+		height: 50px; /* choose any height */
+		background-color:#FFF;
+	}
+  .gsc-input input.gsc-input {
+    background: none repeat scroll 0% 0% white !important;
+  }
+	
+	#gbx3 {
+		left: 0;
+	}
+	#gb.gbesi #gbx3, #gbx3.gbes {
+		height: 29px;
+	}
+	#gbx3 {
+		background-color: #2D2D2D;
+		border-color: #000000;
+		min-width: 980px;
+		opacity: 1;
+	}
+	#gbx3 {
+		left: 0;
+	}
+	#gbx3, #gbx4 {
+		background-color: #2D2D2D;
+		background-image: none;
+		background-position: 0 -138px;
+		background-repeat: repeat-x;
+		border-bottom: 1px solid #000000;
+		font-size: 24px;
+		height: 29px;
+		opacity: 1;
+		position: absolute;
+		top: 0;
+		width: 100%;
+		z-index: 990;
+	}
+	.gbts {
+		color:#FFF;
+		padding-left:20px;
+		font-size: small;
+		font-family: arial,sans-serif;
+		color: #BBBBBB !important;
+    	font-weight: bold;
+	}
+
+	.menu-over{
+		color: #FFFFFF;
+		text-decoration:none;
+	}
+	.menu{ 
+		color: #BBBBBB;
+		text-decoration:none;
+	}
+
+</style> 
+<body>
+<div id="gbx3" class="gbes">
+	<span class="gbts"><a href="http://www.diw.go.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">กรมโรงงานอุตสาหกรรม</a></span>
+    <span class="gbts"><a href="http://www.thaiengineering.com/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">กลุ่มวิศวกรไทย</a></span>
+    <span class="gbts"><a href="http://www.industry.go.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">กระทรวงอุตสาหกรรม</a></span>
+    <span class="gbts"><a href="http://www.dip.go.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">กรมส่งเสริมอุตสาหกรรม</a></span>
+    <span class="gbts"><a href="http://www.oie.go.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">ดัชนีอุตสาหกรรม</a></span>
+    <span class="gbts"><a href="http://www.dbd.go.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">กรมพัฒนาธุรกิจการค้า</a></span>
+    <span class="gbts"><a href="http://www.fti.or.th/" target="_blank" onmouseover="this.className='menu-over'" onmouseout="this.className='menu'" class="menu">สภาอุตสาหกรรมแห่งประเทศไทย</a></span>
+</div>
+<div id="wrapper">
+    <div id="container" style="width:100%; height:100%;">
+        <div id="body" align="center" style="width:100%;">
+            <div>
+                <img src="css/img/logo.png" />
+            </div>
+            <div align="left" style="width:50%; height:100%; margin-left:11%">
+           <script>
+			  (function() {
+				var cx = '011600678515531648799:cjxfxnw43lo';
+				var gcse = document.createElement('script');
+				gcse.type = 'text/javascript';
+				gcse.async = true;
+				gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+					'//www.google.com/cse/cse.js?cx=' + cx;
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(gcse, s);
+			  })();
+			</script>
+            </div>
+            <div id="content" style="width:100%; height:80%;">
+            	<div align="center" style="width:50%; height:80%;">
+                    <gcse:searchbox></gcse:searchbox>
+                </div>
+                <div style="width:80%; text-align:left; height:80%;">
+                    <gcse:searchresults></gcse:searchresults>
+            	</div>
+            <div>
+            
+        </div>
+    </div>
+    <div id="footer" style="border-top:1px solid #CCC;">
+    	<p style="text-align:center; padding-right:20px; padding-top:5px; color:#666; font-size:14px">บริษัท เซฟบาย จำกัด 46 หมู่ 14 นิคมอุตสาหกรรมบางชัน ถ. เสรีไทย แขวงมีนบุรี เขตมีนบุรี กรุงเทพฯ 10510 Tel 02-906-0660, 02-906-0900 Powered By <a href="http://www.savebuy.co.th" target="_parent" style="color:#666; text-decoration:none;">www.savebuy.co.th</a></p>
+    </div>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42141343-1', 'savebuythailand.com');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
